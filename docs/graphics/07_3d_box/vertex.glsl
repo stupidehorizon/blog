31b6ebb92a87813aedc4d8d069f19032b2850332
stupidehorizon <122505231@qq.com>
2020-08-26 19:00:42 +0800

    fix: 3d box color

--- docs/graphics/07_3d_box/vertex.glsl
+++ docs/graphics/07_3d_box/vertex.glsl
@@ -1,8 +1,8 @@
 attribute vec3 a_vertexPosition;
-attribute vec3 color;
+attribute vec3 colors;
 varying vec3 vColor;
 void main() {
   gl_PointSize = 1.0;
-  vColor = color;
+  vColor = colors;
   gl_Position = vec4(a_vertexPosition, 1);
 }
