f7acd729933f3eba662177d9aa4eed11b90ef64b
stupidehorizon <xiexin813@google.com>
2020-08-26 23:21:20 +0800

    alomst finished 3d box

--- docs/graphics/07_3d_box/index.js
+++ docs/graphics/07_3d_box/index.js
@@ -9,84 +9,70 @@ const renderer = new GlRenderer(canvas, {
 const program = renderer.compileSync(fragment, vertex);
 renderer.useProgram(program);
 
+renderer.uniforms.transition = [
+  1,0,0,
+  0,1,0,
+  0,0,1
+]
+
+let startAngle = 0;
+const updateTransition = (angle) => {
+  const c = Math.cos(startAngle += 0.001);
+  const s = Math.sin(startAngle += 0.001);
+  const rotateX = [
+    1,0,0,
+    0,c,s,
+    0,-s,c
+  ];
+  renderer.uniforms.transition = rotateX;
+  requestAnimationFrame(updateTransition);
+}
+requestAnimationFrame(updateTransition);
+
+const colorsBox =  [[1,0,0], [0,1,0], [0,0,1], [1,1,0], [0,1,1], [1,0,1]];
+
+const createBox = (size) => {
+  const h = size/2;
+  const p = [
+    [-h, -h, -h],
+    [-h, h, -h],
+    [h, h, -h],
+    [h, -h, -h],
+    [h, -h, h],
+    [h, h, h],
+    [-h, h, h],
+    [-h, -h, h],
+  ];
+  const positions = [];
+  const cells = [];
+  const colors = [];
+  let cellsLen = 0;
+
+  const createFragement = (a, b, c, d) => {
+    positions.push(p[a], p[b], p[c], p[d]);
+    cells.push([cellsLen + 0, cellsLen + 1, cellsLen + 2], [cellsLen + 0, cellsLen + 2, cellsLen + 3]);
+    const color = colorsBox[(cellsLen/4)%6];
+    colors.push(color, color, color, color);
+    cellsLen+=4;
+  }
+  createFragement(0,1,2,3); 
+  createFragement(3,4,5,2);
+  createFragement(4,5,6,7);
+  createFragement(6,7,0,1);
+  createFragement(1,2,5,6);
+  createFragement(0,3,4,7);
+  return {positions, colors, cells};
+}
+
+const boxData = createBox(1);
+
 renderer.setMeshData([
   {
-    positions: [
-      [-0.5, -0.5, -0.5],
-      [-0.5, 0.5, -0.5],
-      [0.5, 0.5, -0.5],
-      [0.5, -0.5, -0.5],
-      [0.5, -0.5, 0.5],
-      [0.5, 0.5, 0.5],
-      [-0.5, 0.5, 0.5],
-      [-0.5, -0.5, 0.5],
-    ],
+    positions: boxData.positions,
     attributes: {
-      colors: [
-        [1, 0, 0],
-        [1, 0, 0],
-        [1, 0, 0],
-        [1, 0, 0],
-        [0, 1, 0],
-        [0, 1, 0],
-        [0, 1, 0],
-        [0, 1, 0],
-        [0, 0, 1],
-        [0, 0, 1],
-        [0, 0, 1],
-        [0, 0, 1],
-        [1, 0, 0],
-        [1, 0, 0],
-        [1, 0, 0],
-        [1, 0, 0],
-        [0, 1, 0],
-        [0, 1, 0],
-        [0, 1, 0],
-        [0, 1, 0],
-        [0, 0, 1],
-        [0, 0, 1],
-        [0, 0, 1],
-        [0, 0, 1],
-        [1, 0, 0],
-        [1, 0, 0],
-        [1, 0, 0],
-        [1, 0, 0],
-        [0, 1, 0],
-        [0, 1, 0],
-        [0, 1, 0],
-        [0, 1, 0],
-        [0, 0, 1],
-        [0, 0, 1],
-        [0, 0, 1],
-        [0, 0, 1],
-        [1, 0, 0],
-        [1, 0, 0],
-        [1, 0, 0],
-        [1, 0, 0],
-        [0, 1, 0],
-        [0, 1, 0],
-        [0, 1, 0],
-        [0, 1, 0],
-        [0, 0, 1],
-        [0, 0, 1],
-        [0, 0, 1],
-        [0, 0, 1],
-      ]
+      colors: boxData.colors,
     },
-    cells: [
-      [0, 1, 2], // 后
-      [2, 0, 3],
-      [3, 2, 5], // 右
-      [5, 4, 3],
-      [5, 4, 7], // 前
-      [7, 5, 6],
-      [6, 1, 0], // 左
-      [0, 6, 7],
-      [1, 5, 6], // 上
-      [1, 2, 5],
-      [7, 4, 0], // 下
-      [0, 4, 3],
-    ],
+    cells: boxData.cells,
   },
 ]);
 
--- docs/graphics/07_3d_box/vertex.glsl
+++ docs/graphics/07_3d_box/vertex.glsl
@@ -1,8 +1,12 @@
 attribute vec3 a_vertexPosition;
 attribute vec3 colors;
 varying vec3 vColor;
+uniform mat3 transition;
+
+mat3 reverseZ = mat3(1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,-1.0);
+
 void main() {
   gl_PointSize = 1.0;
   vColor = colors;
-  gl_Position = vec4(a_vertexPosition, 1);
+  gl_Position = vec4(reverseZ * transition * vec3(a_vertexPosition), 1);
 }
